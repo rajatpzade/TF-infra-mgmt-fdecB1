0730a3407596f691cf9cc6f0a3a1e56bff923f00
rajatpzade <rajat@gmail.com>
2023-08-09 16:01:19 +0530

    --- Auto Git Commit ---

---
+++ cred.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "eu-west-3"
+  access_key = "AKIAXQ4632KKB7D6LHTO"
+  secret_key = "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"
+}
---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  default = "ami-0b8b5288592eca360"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "az" {
+  default = "eu-west-3b"
+}
+
+# variable "vpc_id" {
+#    default = "vpc-0e95c252ecb9fa5d5" 
+# }
