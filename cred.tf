88ffc93d0196255ec6916fc821f4cf84c6e40068
rajatpzade <rajat@gmail.com>
2023-08-11 10:57:19 +0530

    --- Auto Git Commit ---

--- cred.tf
+++ cred.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "eu-west-3"
   access_key = "AKIA5VR5QTKGJVKMIVEH"
-  secret_key = "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"
+  secret_key = "UqiIljwO9XVGwfu8hrHIAJRhiN2mYPiMnu4yzF1Q"
 }
